8ef599e43d756fb488f6f7c2637b2a405708dd83
maxchernigov <144916762+maxchernigov@users.noreply.github.com>
2023-10-14 11:54:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ class Field:
     def __init__(self, value):
         self.value = value
 
+    
     def __str__(self):
         return str(self.value)
+
 
 class Name(Field):
     def __init__(self, value):
         self.value = value
+
 
 class Phone(Field):
     def __init__(self, value):
@@ -18,14 +21,17 @@ class Phone(Field):
         else:
             raise ValueError("Invalid phone number format")
 
+
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
 
+    
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
-        
+
+    
     def add_phone(self, phone_number):
         try:
             phone = Phone(phone_number)
@@ -34,6 +40,7 @@ class Record:
         except ValueError as e:
             return str(e)
 
+    
     def remove_phone(self, phone_number):
         for phone in self.phones:
             if phone.value == phone_number:
@@ -41,6 +48,7 @@ class Record:
                 return f"Removed phone: {phone_number}"
         raise ValueError(f"Phone {phone_number} not found.")
 
+    
     def edit_phone(self, old_phone_number, new_phone_number):
         for phone in self.phones:
             if phone.value == old_phone_number:
@@ -48,21 +56,23 @@ class Record:
                 return f"Edited phone: {old_phone_number} -> {new_phone_number}"
         raise ValueError(f"Phone {old_phone_number} not found.")
 
+    
     def find_phone(self, phone_number):
         for phone in self.phones:
             if phone.value == phone_number:
                 return phone
         return None
 
-    
 
 class AddressBook(UserDict):
     def add_record(self, record):
         self.data[record.name.value] = record
 
+    
     def find(self, phone_name):
         return self.data.get(phone_name)
 
+    
     def delete(self, name):
             if name in self.data.keys():
                 self.data.pop(name)

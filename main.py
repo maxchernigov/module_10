4475e593e6868874ee0c7bb675716966d074b1eb
maxchernigov <vladsnak23@gmail.com>
2023-10-14 11:41:52 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,76 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if len(value) == 10 and value.isdigit():
+            super().__init__(value)
+        else:
+            raise ValueError("Invalid phone number format")
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+        
+    def add_phone(self, phone_number):
+        try:
+            phone = Phone(phone_number)
+            self.phones.append(phone)
+            return f"Added contact: {phone}"
+        except ValueError as e:
+            return str(e)
+
+    def remove_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                self.phones.remove(phone)
+                return f"Removed phone: {phone_number}"
+        raise ValueError(f"Phone {phone_number} not found.")
+
+    def edit_phone(self, old_phone_number, new_phone_number):
+        for phone in self.phones:
+            if phone.value == old_phone_number:
+                phone.value = new_phone_number
+                return f"Edited phone: {old_phone_number} -> {new_phone_number}"
+        raise ValueError(f"Phone {old_phone_number} not found.")
+
+    def find_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone
+        return None
+
+    
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, phone_name):
+        return self.data.get(phone_name)
+
+    def delete(self, phone_name):
+        if phone_name in self.data:
+            del self.data[phone_name]
+            return f"Deleted contact: {phone_name}"
+        return f"Contact {phone_name} not found."
+
+if __name__ == "__main__":
+    book = AddressBook()
+    
+
+    
+
